f787d3c51549f1ed8f6feb21a762183ff1a014b5
Cooler_ <c00f3r@gmail.com>
2014-05-04 23:24:25 -0300

    Update tombpool.c
    code style update

--- tombpool.c
+++ tombpool.c
@@ -47,256 +47,270 @@ email: marcelo.fleury[at]4linux[dot]com[dot]br
  fprintf(stderr, x, ## s); \
 } while (0);
 
-static int segura=1;
+static int segura = 1;
 
 // morfopara ajudar no lock da fila
-pthread_mutex_t morfo= PTHREAD_MUTEX_INITIALIZER; 
+pthread_mutex_t morfo = PTHREAD_MUTEX_INITIALIZER; 
 
 /* Individual job */
 typedef struct piscina_tarefa{
- void* arg;                    // argumentos da função
- void* (*function)(void* arg);  // ponteiro para função
- struct piscina_tarefa* proximo;   // proxima tarefa              
- struct piscina_tarefa* anterior;  //tarefa anterior          
+	void* arg;                    // argumentos da função
+	void* (*function)(void* arg);  // ponteiro para função
+	struct piscina_tarefa* proximo;   // proxima tarefa              
+	struct piscina_tarefa* anterior;  //tarefa anterior          
 }piscina_tarefa;
 
 typedef struct piscina_fila{
- int NumTarefa;         // tarefas na fila
- sem_t *filaSemaforo;  // semaforo para controlar as tarefas
- piscina_tarefa *start;  // inicio da fila
- piscina_tarefa *end; // final da fila
+	int NumTarefa;         // tarefas na fila
+	sem_t *filaSemaforo;  // semaforo para controlar as tarefas
+	piscina_tarefa *start;  // inicio da fila
+	piscina_tarefa *end; // final da fila
 }piscina_fila;
 
 // struct da thread pool
 typedef struct piscina{
- int NumThread;   // num threads
- pthread_t* threads;              
- piscina_fila* TarefaFila; // ponteiro apra tarefa
+	int NumThread;   // num threads
+	pthread_t* threads;              
+	piscina_fila* TarefaFila; // ponteiro apra tarefa
 }piscina;
 
 // ponte de dados
 typedef struct thread_data{                            
- pthread_mutex_t *mutex_p;
- piscina        *dados;
+	pthread_mutex_t *mutex_p;
+	piscina        *dados;
 }thread_data;
 
 
 // HEAP alignment :-X
 void *xmalloc(unsigned int len)
 {
- void *ptr;
- ptr=malloc(len);
- if(ptr==NULL)
-  DEBUG("fail malloc");
- return ptr;
+	void *ptr;
+	
+	ptr = malloc ( len );
+	if ( ptr == NULL )
+		DEBUG("fail malloc");
+		
+	return ptr;
 }
 
 // enche a piscina kkkkk
-piscina* Dig_TombPool(int NumThread)
+piscina *Dig_TombPool( int NumThread )
 {
- piscina* dados;
- int count=0;
+	piscina* dados;
+	int count = 0;
 	
- if(NumThread<1) 
-  NumThread=1;
+	if( NumThread < 1 ) 
+		NumThread = 1;
 // usamos a heap para criar a pool
- dados=(piscina*)xmalloc(sizeof(piscina));                             
+	dados = (piscina*) xmalloc ( sizeof( piscina ) );                             
 
 // alocamos apra ID agora
- dados->threads=(pthread_t*)xmalloc(NumThread*sizeof(pthread_t));      
- dados->NumThread=NumThread;
+	dados->threads = (pthread_t*) xmalloc ( NumThread * sizeof(pthread_t) );      
+	dados->NumThread = NumThread;
 	
 // iniciamos tarefa de fila
- piscina_fila_init(dados);
+	piscina_fila_init( dados );
 	
 // alocamos para o semaforo
- dados->TarefaFila->filaSemaforo=(sem_t*)xmalloc(sizeof(sem_t));                 
- sem_init(dados->TarefaFila->filaSemaforo, 0, 0); 
+	dados->TarefaFila->filaSemaforo = (sem_t*) xmalloc ( sizeof( sem_t ) );                 
+	sem_init( dados->TarefaFila->filaSemaforo , 0, 0 ); 
 	
 // damos create com pthread
 
- do{
-  pthread_create(&(dados->threads[count]), NULL, (void *)poolWheel, (void *)dados); 
-  count++;
- }while(count<NumThread);
+	do{
+		
+		pthread_create ( & ( dados->threads[count] ), NULL , (void *) poolWheel , (void *) dados ); 
+		count++;
+		
+	}while( count < NumThread );
 	
- return dados;
+	
+	return dados;
 }
 
 // roda a fila
-void poolWheel(piscina* dados)
+void poolWheel( piscina* dados )
 {
- do{
+	do{
 //espera ateh que tenha trabalho na fila		
-  sem_wait(dados->TarefaFila->filaSemaforo);
+		sem_wait(dados->TarefaFila->filaSemaforo);
    
-  if(segura)
-  {			
+		if ( segura )
+		{			
 // passa tarefa para fila e executa
 // Hic Sunt Dracones
-   void*(*func_buff)(void* arg);
-   void* arg_buff;
+			void *(*func_buff) (void* arg);
+			void* arg_buff;
 
-   piscina_tarefa* job_p;
+			piscina_tarefa* job_p;
 	
-   pthread_mutex_lock(&morfo);               			
-   job_p = piscina_fila_peek(dados);
-   if(job_p == NULL)
-   {
-    piscina_fila_removelast(dados);
-    pthread_mutex_unlock(&morfo);
-    return;
-   }
-   func_buff=job_p->function;
-   arg_buff=job_p->arg;
-   piscina_fila_removelast(dados);
-   pthread_mutex_unlock(&morfo);                
+			pthread_mutex_lock( &morfo );               			
+			job_p = piscina_fila_peek(dados);
+			if ( job_p == NULL )
+			{
+				piscina_fila_removelast( dados );
+				pthread_mutex_unlock( &morfo );
+				return;
+			}
+			
+			func_buff = job_p->function;
+			arg_buff = job_p->arg;
+			piscina_fila_removelast( dados );
+			pthread_mutex_unlock( &morfo );                
 // roda a função			
-   func_buff(arg_buff);               			
-   free(job_p);   
-   segura--;                                                    
-  }
-  else
-  {
-  return; 
-  }
- }while(segura);
- return;
+			func_buff( arg_buff );               			
+			free( job_p );   
+			segura--;                                                    
+		}
+		else
+		{
+			return; 
+		}
+		
+	}while ( segura );
+	
+	return;
 }
 
 
 // adiciona um trabalho na pool
 int Add_Corpse(piscina* dados, void *(*function_p)(void*), void* arg_p)
 {
- piscina_tarefa* newJob;
- newJob=(piscina_tarefa*)xmalloc(sizeof(piscina_tarefa));                     
+	piscina_tarefa* newJob;
+	newJob = (piscina_tarefa*) xmalloc( sizeof(piscina_tarefa) );                     
 	
 // adiciona a função e o argumento
- newJob->function=function_p;
- newJob->arg=arg_p;
+	newJob->function = function_p;
+	newJob->arg = arg_p;
 	
 // adiciona tarefa na fila e logo o lock para evitar reentrancia
- pthread_mutex_lock(&morfo);          
- piscina_fila_add(dados, newJob);
- pthread_mutex_unlock(&morfo);                
+	pthread_mutex_lock( &morfo );          
+	piscina_fila_add( dados, newJob );
+	pthread_mutex_unlock( &morfo );                
 	
- return 0;
+	return 0;
 }
 
 
 // executa as thread e limpa tudo
-void Cover_TombPool(piscina* dados,int NumThread)
+void Cover_TombPool ( piscina* dados, int NumThread)
 {
- int count=0;
+	int count = 0;
 // acabar com thread que nao sai de loop
- segura=NumThread; 
+	segura = NumThread; 
 
 // threads atuais esperam o semaforo
- do{
-  if(sem_post(dados->TarefaFila->filaSemaforo))
-   DEBUG("not bypass semafore wait,car crash xD!\n");
-  count++;
- }while(count<(dados->NumThread));
+	do{
+		
+		if ( sem_post(dados->TarefaFila->filaSemaforo) )
+			DEBUG ( "not bypass semafore wait,car crash xD!\n" );
+		count++;
+		
+	}while (count < (dados->NumThread) );
 
 
  count=0;
 // acabamos e executamos
- do{
-  pthread_join(dados->threads[count], NULL);
-  count++;
- }while(count<(dados->NumThread));
+	do{
+		pthread_join( dados->threads[count] , NULL );
+		count++;
+		
+	}while( count < (dados->NumThread) );
 
 
 // destroi o semaforo
- if(sem_destroy(dados->TarefaFila->filaSemaforo)!=0)
-  DEBUG("error in destroy semaphore\n");
+	if(sem_destroy( dados->TarefaFila->filaSemaforo ) != 0 )
+		DEBUG( "error in destroy semaphore\n" );
 
 // liberamos a heap
- free(dados->threads);                                           
- free(dados->TarefaFila->filaSemaforo);                                        
- free(dados->TarefaFila);                                                  
- free(dados);                                                            
+	free( dados->threads );                                           
+	free( dados->TarefaFila->filaSemaforo );                                        
+	free( dados->TarefaFila );                                                  
+	free( dados );                                                            
 }
 
 
 // inicia fila , aloca heap...
-int piscina_fila_init(piscina* dados)
+int piscina_fila_init( piscina* dados)
 {
- dados->TarefaFila=(piscina_fila*)xmalloc(sizeof(piscina_fila));      
- dados->TarefaFila->end=NULL;
- dados->TarefaFila->start=NULL;
- dados->TarefaFila->NumTarefa=0;
+	
+	dados->TarefaFila=(piscina_fila*) xmalloc( sizeof(piscina_fila) );      
+	dados->TarefaFila->end = NULL;
+	dados->TarefaFila->start = NULL;
+	dados->TarefaFila->NumTarefa = 0;
 
- return 0;
+	return 0;
 }
 
 
 // adiciona tarefa na fila
-void piscina_fila_add(piscina* dados, piscina_tarefa* newjob_p)
+void piscina_fila_add( piscina* dados , piscina_tarefa* newjob_p)
 { 
- int x=0;
+	int x = 0;
 // setamos para null parte duplamente encadeada
- newjob_p->proximo=NULL;
- newjob_p->anterior=NULL;
- piscina_tarefa *oldFirstJob;
- oldFirstJob = dados->TarefaFila->start;
+	newjob_p->proximo = NULL;
+	newjob_p->anterior = NULL;
+	piscina_tarefa *oldFirstJob;
+	oldFirstJob = dados->TarefaFila->start;
 	
 // ajusta os ponteiros das tarefas
- switch(dados->TarefaFila->NumTarefa)
- {
+	switch( dados->TarefaFila->NumTarefa )
+	{
 // caso nao tenha tarefas na fila
-  case 0:
-   dados->TarefaFila->end=newjob_p;
-   dados->TarefaFila->start=newjob_p;
-   break;
+		case 0:
+			dados->TarefaFila->end = newjob_p;
+			dados->TarefaFila->start = newjob_p;
+			break;
 
 // caso já tenha tarefas na fila		
-  default: 	
-   oldFirstJob->anterior=newjob_p;
-   newjob_p->proximo=oldFirstJob;
-   dados->TarefaFila->start=newjob_p;
- }
+		default: 	
+			oldFirstJob->anterior = newjob_p;
+			newjob_p->proximo = oldFirstJob;
+			dados->TarefaFila->start = newjob_p;
+	}
 
- (dados->TarefaFila->NumTarefa)++;     
- sem_post(dados->TarefaFila->filaSemaforo);
+	(dados->TarefaFila->NumTarefa)++;     
+	sem_post( dados->TarefaFila->filaSemaforo );
 	
- sem_getvalue(dados->TarefaFila->filaSemaforo, &x);
+	sem_getvalue(dados->TarefaFila->filaSemaforo, &x);
 }
 
 
 // remove tarefa da fila
-int piscina_fila_removelast(piscina* dados)
+int piscina_fila_removelast( piscina* dados )
 {
- piscina_tarefa *oldLastJob;
- oldLastJob = dados->TarefaFila->end;
- int x=0;
+	
+	piscina_tarefa *oldLastJob;
+	oldLastJob = dados->TarefaFila->end;
+	int x = 0;
 	
 // ajustando tarefas
- switch(dados->TarefaFila->NumTarefa)
- {	
+	switch( dados->TarefaFila->NumTarefa )
+	{	
 // caso tenha alguma tarefa na fila	
-  case 0:     
-   return -1;
-   break;
+		case 0:     
+			return -1;
+			break;
 // se tem apenas uma tarefa na fila		
-  case 1:     
-   dados->TarefaFila->end=NULL;
-   dados->TarefaFila->start=NULL;
-   break;
+		case 1:     
+			dados->TarefaFila->end = NULL;
+			dados->TarefaFila->start = NULL;
+			break;
 // caso tenha mais de uma tarefa na fila					
-  default:
-   oldLastJob->anterior->proximo=NULL;               
-   dados->TarefaFila->end=oldLastJob->anterior;
- }
- (dados->TarefaFila->NumTarefa)--;
- sem_getvalue(dados->TarefaFila->filaSemaforo, &x);
+		default:
+			oldLastJob->anterior->proximo = NULL;               
+			dados->TarefaFila->end = oldLastJob->anterior;
+	}
+	(dados->TarefaFila->NumTarefa)--;
+	sem_getvalue( dados->TarefaFila->filaSemaforo , &x );
 
- return 0;
+	return 0;
 }
 
 
 // pega primeiro elemento da fila
 piscina_tarefa* piscina_fila_peek(piscina* dados)
 {
- return dados->TarefaFila->end;
+	
+	return dados->TarefaFila->end;
 }
