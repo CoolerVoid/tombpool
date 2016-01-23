a2ba09fcb60d5eba1e8d5abbd674711458f8cefb
Cooler_ <coolerlair@gmail.com>
2016-01-23 01:47:19 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ echo "Tomb Pool - https://github.com/CoolerVoid/tombpool "
 mkdir pack; cd pack
 cmake ../; make; cd .. ; cp tombpool.h pack; cp test.c pack; cd pack
 cp libtombpool.so /lib
+cp libtombpool.so /usr/local/lib
+cp libtombpool.so /usr/lib64/
+cp libtombpool.so /usr/lib32/
+cp libtombpool.so /usr/lib
+
 echo "================================================"
 echo "Test programm at pack/test\n "
 echo " $ gcc -o test test.c -lpthread -ltombpool"
